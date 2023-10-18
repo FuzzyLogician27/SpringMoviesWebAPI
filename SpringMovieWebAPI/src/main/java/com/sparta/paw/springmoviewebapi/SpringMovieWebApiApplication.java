17fdb5a3652bbc206f0ba56fa5bf348a4978766e
FuzzyLogician27 <p.ardaghwalter@hotmail.com>
2023-10-18 15:49:31 +0100

    Initial Entity Setup

---
+++ SpringMovieWebAPI/src/main/java/com/sparta/paw/springmoviewebapi/SpringMovieWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.paw.springmoviewebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMovieWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMovieWebApiApplication.class, args);
+	}
+
+}
---
+++ SpringMovieWebAPI/src/test/java/com/sparta/paw/springmoviewebapi/SpringMovieWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.paw.springmoviewebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMovieWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
